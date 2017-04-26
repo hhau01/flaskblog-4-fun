8b20a65e1db4e1d8e112e297188107712c26b286
Henry Au <henryau01@gmail.com>
2017-04-25 22:17:56 -0700

    initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe repudiandae quos ut, natus esse corporis. Exercitationem, alias officiis! In quidem laborum, aliquid nostrum ipsum consequuntur magnam id exercitationem. Quae, laborum.',
+            'author': 'Henry Au',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe repudiandae quos ut, natus esse corporis. Exercitationem, alias officiis! In quidem laborum, aliquid nostrum ipsum consequuntur magnam id exercitationem. Quae, laborum.',
+            'author': 'Bilbo Baggins',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe repudiandae quos ut, natus esse corporis. Exercitationem, alias officiis! In quidem laborum, aliquid nostrum ipsum consequuntur magnam id exercitationem. Quae, laborum.',
+            'author': 'Darth Vader',
+            'create_date': '04-25-2017'
+        }
+    ]
+    return articles
