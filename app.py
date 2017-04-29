de32f79e2a1a0b408c11875661e64e8e577683b8
Henry Au <henryau01@gmail.com>
2017-04-29 13:25:13 -0700

    login and access controls

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -18,22 +19,27 @@ mysql = MySQL(app)
 
 Articles = Articles()
 
+# HOME
 @app.route('/')
 def index():
     return render_template('home.html')
 
+# ABOUT
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# ARTICLES
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles=Articles)
 
+# SINGLE ARTICLE
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
 
+# REGISTER FORM CLASS
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -44,6 +50,7 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
+# USER REGISTER
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -70,6 +77,71 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
+
+# user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # create database cursor
+        cur = mysql.connection.cursor()
+
+        # get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # get stored hash; fetchone retrieves first instance
+            data = cur.fetchone()
+            password = data['password']
+
+            # compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # app.logger.info('PASSWORD MATCH')
+
+                # PASSED
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = "Invalid login"
+                return render_template('login.html', error=error)
+
+            # close connection
+            cur.close()
+        else:
+            error = "Username not found"
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+# CHECK IF USER IS LOGGED IN
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please log in', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# LOGOUT
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# DASHBOARD
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.secret_key='secret12345'
